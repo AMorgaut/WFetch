8b4d443b5d9048ee531622c8518d0ae3c08eb9bd
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2015-03-12 09:41:04 +0100

    Fetch Wakanda Custom Widget Package creation

---
+++ designer.js
@@ -0,0 +1,40 @@
+(function(Fetch) {
+
+//    /* Default width and height of your widget */
+//    Fetch.setWidth('200');
+//    Fetch.setHeight('20');
+
+//    /* Define custom event for your widget */
+//    Fetch.addEvent('myEvent');
+
+//    /* Customize existing properties */
+//    Fetch.customizeProperty('test', {
+//        title: 'Test Static Value',
+//        description: 'Add a datasource to this property.'
+//    });
+
+//    /* Add a Label property */
+//    Fetch.addLabel({
+//        'defaultValue': '',
+//        'position': 'top'
+//    });
+
+//    /* Set the Design and Styles panels */
+//    Fetch.setPanelStyle({
+//        'fClass': true,
+//        'text': true,
+//        'background': true,
+//        'border': true,
+//        'sizePosition': true,
+//        'label': true,
+//        'disabled': ['border-radius']
+//    });
+
+//    /* Override widget's initialization */
+//    Fetch.prototype.init = function() {
+//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+//    }
+
+});
+
+// For more information, refer to http://doc.wakanda.org/Wakanda/help/Title/en/page3870.html
---
+++ widget.js
@@ -0,0 +1,29 @@
+WAF.define('Fetch', ['waf-core/widget'], function(widget) {
+	
+    var Fetch = widget.create('Fetch', {
+        init: function() {
+//            /* Define a custom event */
+//            this.fire('myEvent', {
+//                message: 'Hello'
+//            });
+        }
+//        ,
+        
+//        /* Create a property */
+//        test: widget.property({
+//            onChange: function(newValue) {
+//                this.node.innerHTML = this.test(); /* this contains the widget and newValue contains its current value */
+//            }
+//        })
+    });
+
+//    /* Map the custom event above to the DOM click event */
+//    Fetch.mapDomEvents({
+//        'click': 'action'
+//    });
+
+    return Fetch;
+
+});
+
+/* For more information, refer to http://doc.wakanda.org/Wakanda/help/Title/en/page3871.html */
